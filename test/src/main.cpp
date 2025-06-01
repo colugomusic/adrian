0f28336a074cecf4c0ba347f508c5ef62c801894
Colugo <colugomusic@gmail.com>
2025-06-01 11:01:18 +0100

    Update main.cpp

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -3,4 +3,11 @@
 #include "doctest.h"
 
 TEST_CASE("test test") {
+	auto options = adrian::chain_options{};
+	options.allocate_now   = true;
+	options.enable_mipmaps = false;
+	options.silent         = false;
+	auto idx = adrian::make_chain(ez::ui, ads::channel_count{2}, ads::frame_count{1024}, options, {});
+	REQUIRE(bool(idx));
+	adrian::erase(ez::ui, idx);
 }
