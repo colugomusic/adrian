d0eaa5305fdfee5485cff96d608182f6a59ae30c
Colugo <colugomusic@gmail.com>
2025-07-09 08:57:00 +0100

    Update adrian-pp.hpp

--- include/adrian-pp.hpp
+++ include/adrian-pp.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-#define DEFAULT_EQUALITY(x)  [[nodiscard]] auto operator==(const x&) const -> bool = default
-#define DEFAULT_SPACESHIP(x) [[nodiscard]] auto operator<=>(const x&) const = default
+#define DEFAULT_EQUALITY(x)  [[nodiscard]] friend auto operator==(const x&, const x&) -> bool = default
+#define DEFAULT_SPACESHIP(x) [[nodiscard]] friend auto operator<=>(const x&, const x&) = default
