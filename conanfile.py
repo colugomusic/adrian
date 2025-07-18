1f5b4c303b682641ec1d9f198a435bf9c47ed2e6
Colugo <colugomusic@gmail.com>
2025-07-18 16:14:55 +0100

    add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import check_min_cppstd
+
+class AdrianConanFile(ConanFile):
+	name = "adrian"
+	version = "1.0.0"
+	license = "MIT"
+	author = "ColugoMusic"
+	url = "https://github.com/colugomusic/adrian"
+	settings = "os", "compiler", "build_type", "arch"
+	generators = "CMakeDeps", "CMakeToolchain"
+	exports_sources = "CMakeLists.txt", "include/*"
+
+	def layout(self):
+		cmake_layout(self)
+
+	def requirements(self):
+		self.requires("ads/[>=1.0]")
+		self.requires("ez/[>=1.0]")
+		self.requires("readerwriterqueue/1.0.6")
+		self.requires("immer/0.8.1")
+		self.requires("madronalib/[>=0]")
+
+	def validate(self):
+		if self.settings.get_safe("compiler.cppstd"):
+			check_min_cppstd(self, 20)
+
+	def build(self):
+		cmake = CMake(self)
+		cmake.configure()
+		cmake.build()
+
+	def package(self):
+		cmake = CMake(self)
+		cmake.install()
+	
+	def package_info(self):
+		self.cpp_info.includedirs = ["include"]
