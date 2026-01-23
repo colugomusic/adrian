482c6ffe972dfb992cf9e48c8e8332dc9619842b
Colugo <colugomusic@gmail.com>
2026-01-23 12:57:43 +0000

    Update jthread.hpp

--- extern/jthread/jthread.hpp
+++ extern/jthread/jthread.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#if defined(__APPLE__)
+#if defined(__apple_build_version__)
 #include "jthread-impl.hpp"
 namespace std { using jthread = nonstd::jthread; using stop_token = nonstd::stop_token; }
 #else
