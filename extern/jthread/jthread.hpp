9545e5d70d4846bdbd7331c8dcc4dc94a5549297
Colugo <colugomusic@gmail.com>
2026-02-06 15:50:18 +0000

    Update jthread.hpp

--- extern/jthread/jthread.hpp
+++ extern/jthread/jthread.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
-#if defined(__apple_build_version__)
+#include <version>
+
+// Use the fallback jthread implementation if std::jthread is not available.
+// This can happen with:
+//   - AppleClang (doesn't support jthread)
+//   - LLVM Clang with libc++ targeting older macOS (jthread requires macOS 14+)
+#if defined(__cpp_lib_jthread) && __cpp_lib_jthread >= 201911L
+#include <thread>
+#else
 #include "jthread-impl.hpp"
 namespace std { using jthread = nonstd::jthread; using stop_token = nonstd::stop_token; }
-#else
-#include <thread>
 #endif
